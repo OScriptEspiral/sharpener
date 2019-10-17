4b22913cc33a1b1a805d309317f72735d25cbeb6
Pedro Morello Abbud <abbudao@gmail.com>
2019-10-17 13:46:16 -0300

    Healthcheck + Continous Delivery setup

--- sharpener-admin.py
+++ sharpener-admin.py
@@ -11,6 +11,7 @@ from docopt import docopt
 from dynaconf import settings
 from google.cloud import storage
 from sqlalchemy import create_engine, orm
+from sqlalchemy.engine import url
 from connectors import exercism
 from models import Base
 from api import create_app
@@ -21,13 +22,43 @@ def create_db(db_uri):
     return Base.metadata.create_all(engine)
 
 
-def database_setup(db_uri):
-    engine = create_engine(db_uri, client_encoding="utf8", echo=True)
+def database_setup(api, username, password, name, conn_name=None):
+    unix_socket = f'/cloudsql/{conn_name}'
+    print(url.URL(
+            drivername=api,
+            username=username,
+            password=password,
+            database=name,
+            query={
+                'host': unix_socket
+            }))
+    engine = create_engine(
+        url.URL(
+            drivername=api,
+            username=username,
+            password=password,
+            database=name,
+            query={
+                'host': unix_socket
+            }),
+        client_encoding="utf8")
+
     sess = orm.sessionmaker(bind=engine)
     return sess()
 
 
-session = database_setup(f"{settings.DB_API}{settings.DB_URI}")
+try:
+    import googleclouddebugger
+    googleclouddebugger.enable()
+except ImportError:
+    pass
+
+session = database_setup(settings.DB_API,
+                         settings.DB_USERNAME,
+                         settings.DB_PASSWORD,
+                         settings.DB_NAME,
+                         settings.DB_CONN_NAME)
+
 app = create_app(session)
 
 if __name__ == "__main__":
